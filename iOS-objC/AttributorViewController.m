49c4e1b667d89e575ae3adc5d0e5ae70e5d7c719
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-08-07 11:53:55 +0200

    Lecture: 5

---
+++ iOS-objC/AttributorViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  iOS-objC
+//
+//  Created by ivica petrsoric on 02/08/2019.
+//  Copyright © 2019 ivica petrsoric. All rights reserved.
+//
+
+#import "AttributorViewController.h"
+
+@interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
+@end
+
+@implementation AttributorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSMutableAttributedString *title =
+    [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{ NSStrokeWidthAttributeName: @3,
+                            NSStrokeColorAttributeName: self.outlineButton.tintColor }
+                   range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:normal];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self userPreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)preferredFontsChanged: (NSNotification *)notification {
+    [self userPreferredFonts];
+}
+
+- (void)userPreferredFonts {
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (IBAction)changeBodySelectionColorToMachBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                                           NSStrokeColorAttributeName: [UIColor blackColor]}
+                                  range: self.body.selectedRange];
+     
+}
+
+- (IBAction)unoutlineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
+
+@end
