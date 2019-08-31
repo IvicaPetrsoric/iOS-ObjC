0829cb4f3a88cbee3f84d3dac4e2951ea21e5665
ivica petrsoric <ivica.petrsoric@codecacao.com>
2019-08-31 14:56:07 +0200

    Lecture: 2

--- iOS-objC/ViewController.m
+++ iOS-objC/ViewController.m
@@ -9,15 +9,34 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (weak, nonatomic) IBOutlet UISlider *mySlider;
 
 @end
 
 @implementation ViewController
 
+int counter;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    counter = 0;
+    self.myLabel.text = [[NSNumber numberWithInt:counter] stringValue];
+    
+}
+- (IBAction)buttonPressed:(UIButton *)sender {
+    counter++;
+    
+    _myLabel.text = [[NSNumber numberWithInt:counter] stringValue];
+    
+    _mySlider.value = counter;
 }
 
+- (IBAction)sliderMoved:(id)sender {
+    counter = _mySlider.value;
+    
+    _myLabel.text = [[NSNumber numberWithInt:counter] stringValue];
+}
 
 @end
